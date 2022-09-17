dfa8048366b5874d4f2ef3bec8fb6132b7b1d462
vscing <vscing@foxmail.com>
2022-09-17 13:37:22 +0800

    ✨ feat: 商家端

---
+++ src/views/sys/shop/store/Add.vue
@@ -0,0 +1,165 @@
+<template>
+  <PageWrapper title="基础表单" contentBackground contentClass="p-4">
+    <Form ref="formRef" :model="formData" :label-col="labelCol" :wrapper-col="wrapperCol" :rules="rules">
+      <FormItem label="商品标题" name="title">
+        <Input v-model:value="formData.title" placeholder="请输入" />
+      </FormItem>
+      <FormItem label="商品封面图" name="img">
+        <Upload name="file" list-type="picture-card" :show-upload-list="false" :action="uploadUrl" :headers="headers"
+          :before-upload="beforeUpload" @change="(info) => handleChange(info)">
+          <Image v-if="formData.img" :src="formData.img" alt="avatar" />
+          <div v-else>
+            <LoadingOutlined v-if="loading" />
+            <PlusOutlined v-else />
+            <div>点击上传</div>
+          </div>
+        </Upload>
+      </FormItem>
+      <FormItem label="商品价格" name="price">
+        <Input v-model:value="formData.price" placeholder="请输入" />
+      </FormItem>
+      <FormItem label="商品预售数量" name="total_stock">
+        <Input v-model:value="formData.total_stock" placeholder="请输入" />
+      </FormItem>
+      <FormItem label="商品预售时间" name="presell_time">
+        <DatePicker v-model:value="formData.presell_time" show-time placeholder="请选择" format="YYYY-MM-DD hh:mm:ss"/>
+      </FormItem>
+      <FormItem label="合约地址" name="contract_address">
+        <Input v-model:value="formData.contract_address" placeholder="请输入" />
+      </FormItem>
+      <FormItem label="认证标识" name="token_id">
+        <Input v-model:value="formData.token_id" placeholder="请输入" />
+      </FormItem>
+      <FormItem label="认证标准" name="token_standard">
+        <Input v-model:value="formData.token_standard" placeholder="请输入" />
+      </FormItem>
+      <FormItem label="认证网络" name="blockchain">
+        <Input v-model:value="formData.blockchain" placeholder="请输入" />
+      </FormItem>
+      <FormItem label="商品简介" name="desc">
+        <Input.TextArea v-model:value="formData.desc" :rows="5" placeholder="请输入" :maxlength="200" />
+      </FormItem>
+      <FormItem label="商品描述" name="content">
+        <Tinymce v-model:value="formData.content" @change="(value) => formData.content = value" />
+      </FormItem>
+      <FormItem label="排序">
+        <Input v-model:value="formData.sort" placeholder="请输入" />
+      </FormItem>
+      <FormItem :wrapper-col="{ span: 14, offset: 4 }">
+        <Button @click="resetForm">取消</Button>
+        <Button style="margin-left: 10px" type="primary" @click="onSubmit">确定</Button>
+      </FormItem>
+    </Form>
+  </PageWrapper>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import { PageWrapper } from '/@/components/Page';
+import { Form, FormItem, Input, Upload, Image, Button, DatePicker } from 'ant-design-vue';
+import { Tinymce } from '/@/components/Tinymce/index';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons-vue';
+import { useMessage } from '/@/hooks/web/useMessage';
+import { useGlobSetting } from '/@/hooks/setting';
+import { getToken } from '/@/utils/auth';
+import { formatToDateTime, getDate } from '/@/utils/dateUtil';
+import { addGoodsInfo } from '/@/api/sys/goods';
+
+const router = useRouter();
+
+const labelCol = { span: 4 };
+const wrapperCol = { span: 14 };
+
+const { createMessage } = useMessage();
+const { uploadUrl } = useGlobSetting();
+
+const token = getToken();
+const loading = ref<boolean>(false);
+const headers = ref({
+  Authorization: `Bearer ${token}`
+})
+
+const beforeUpload = (file) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/gif';
+  if (!isJpgOrPng) {
+    createMessage.error('上传图片格式不正确');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 50;
+  if (!isLt2M) {
+    createMessage.error('请上传50MB以下图片!');
+  }
+  return isJpgOrPng && isLt2M;
+}
+
+const handleChange = (info: any) => {
+  const file = info.file;
+  const status = file?.status;
+  const url = file?.response?.result?.url;
+
+  if (status === 'uploading') {
+    if (!loading.value) {
+      loading.value = true;
+    }
+  } else if (status === 'done') {
+    formData.img = url;
+    loading.value = false;
+  } else if (status === 'error') {
+    loading.value = false;
+    createMessage.error('上传失败');
+  }
+}
+
+const formRef = ref();
+
+const rules = {
+  title: [
+    { required: true, message: '请输入', trigger: 'blur' },
+  ],
+  img: [{ required: true, message: '请选择', trigger: 'change' }],
+  presell_time: [{ required: true, message: '请选择', trigger: 'change' }],
+  price: [{ required: true, message: '请输入', trigger: 'blur' }],
+  total_stock: [{ required: true, message: '请输入', trigger: 'blur' }],
+  contract_address: [{ required: true, message: '请输入', trigger: 'blur' }],
+  token_id: [{ required: true, message: '请输入', trigger: 'blur' }],
+  token_standard: [{ required: true, message: '请输入', trigger: 'blur' }],
+  blockchain: [{ required: true, message: '请输入', trigger: 'blur' }],
+  desc: [{ required: true, message: '请输入', trigger: 'blur' }],
+};
+
+const formData = reactive<any>({
+  title: '',
+  img: '',
+  price: '0.00',
+  total_stock: '1',
+  presell_time: getDate(),
+  contract_address: '',
+  token_id: '',
+  token_standard: '',
+  blockchain: '',
+  desc: '',
+  content: '',
+  sort: '0',
+});
+
+
+const onSubmit = () => {
+  formRef.value
+    .validate()
+    .then(async () => {
+      const res = await addGoodsInfo({...formData, presell_time: formatToDateTime(formData.presell_time)});
+      console.log('%c [ res ]-150', 'font-size:13px; background:pink; color:#bf2c9f;', res)
+      if(res) {
+        createMessage.success('新增成功');
+        router.replace('/goods/sell');
+      }
+    })
+    .catch((error: any) => {
+      console.log('error', error);
+    });
+}
+
+const resetForm = () => {
+  formRef.value.resetFields();
+};
+</script>  
---
+++ src/views/sys/shop/store/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <PageWrapper dense contentFullHeight fixedHeight contentClass="flex">
+    <BasicTable @register="registerTable" :searchInfo="searchInfo">
+      <template #toolbar>
+        <a-button type="primary" @click="handleCreate">新增发售商品</a-button>
+      </template>
+      <template #img="{ record }">
+        <AntImage
+          :width="53"
+          :src="record.img"
+        />
+      </template>
+      <template #status="{ record }">
+        <span>{{ getStatusText(record.status)}}</span>
+      </template>
+      <template #presell_time="{ record }">
+        <span>{{ columnToDateTime(record.presell_time) }}</span>
+      </template>
+      <template #created_at="{ record }">
+        <span>{{ columnToDateTime(record.created_at) }}</span>
+      </template>
+      <template #action="{ record }">
+        <TableAction :actions="[
+          {
+            icon: 'clarity:note-edit-line',
+            tooltip: '编辑',
+            onClick: handleEdit.bind(null, record),
+          },
+          {
+            icon: 'ant-design:delete-outlined',
+            color: 'error',
+            tooltip: '删除',
+            popConfirm: {
+              title: '是否确认删除',
+              confirm: handleDelete.bind(null, record),
+            },
+          },
+        ]" />
+      </template>
+    </BasicTable>
+  </PageWrapper>
+</template>
+<script lang="ts" setup>
+import { reactive } from 'vue';
+
+import { Image as AntImage } from 'ant-design-vue';
+import { BasicTable, useTable, TableAction } from '/@/components/Table';
+import { getGoodsList } from '/@/api/sys/goods';
+import { PageWrapper } from '/@/components/Page';
+import { useMessage } from '/@/hooks/web/useMessage';
+
+import { columns, searchFormSchema } from './data';
+import { useGo } from '/@/hooks/web/usePage';
+import { columnToDateTime } from '/@/utils/dateUtil';
+
+const go = useGo();
+const { createMessage } = useMessage();
+const searchInfo = reactive<Recordable>({});
+// , { reload, updateTableDataRecord }
+const [registerTable] = useTable({
+  title: '预售产品列表',
+  api: getGoodsList,
+  rowKey: 'id',
+  columns,
+  formConfig: {
+    labelWidth: 120,
+    schemas: searchFormSchema,
+    autoSubmitOnEnter: true,
+  },
+  useSearchForm: true,
+  showTableSetting: true,
+  bordered: true,
+  handleSearchInfoFn(info) {
+    console.log('handleSearchInfoFn', info);
+    return info;
+  },
+  actionColumn: {
+    width: 120,
+    title: '操作',
+    dataIndex: 'action',
+    slots: { customRender: 'action' },
+  },
+});
+
+const getStatusText = (status) => {
+  let text = '';
+  if(status == 10) {
+    text = '待上架';
+  } else if(status == 20){
+    text = '预售中';
+  } else if(status == 30){
+    text = '售卖中';
+  } else if(status == 40){
+    text = '已售罄';
+  }
+  return text;  
+}
+
+const handleCreate = () => {
+  go('/goods/sell_add')
+}
+
+function handleEdit(record: Recordable) {
+  go(`/goods/sell_edit/${record.id}`)
+}
+
+function handleDelete(record: Recordable) {
+  console.log(record);
+  createMessage.warning('暂不能删除');
+}
+</script>
